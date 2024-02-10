30a6850b3ddb7d1b9c0eda509a6cd455740e9e66
wwnOOv <145524959+wwnoov@users.noreply.github.com>
2024-02-11 00:44:09 +0900

    react pratice

---
+++ MyName2.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyName2 = ({ name }) => {
+    return (
+        <div>
+            Hi! My name is {name}.
+        </div>
+    );
+};
+
+export default MyName2;
